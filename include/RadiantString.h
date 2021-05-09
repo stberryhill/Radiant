672dca32cb5ff5f8519c89375e54e2527914043a
S. Tyler Berryhill <sean.tyler.berryhill@gmail.com>
2021-05-08 22:09:20 -0500

    Feat: Add string functions.

---
+++ include/RadiantString.h
@@ -0,0 +1,14 @@
+#ifndef _RADIANT_STRING_H
+#define _RADIANT_STRING_H
+
+#include "RadiantAPIMacro.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+RADIANT_API bool String_Equal(const char *a, const char *b);
+RADIANT_API void String_Copy(const char *src, char *dest);
+RADIANT_API void String_CopyN(const char *src, char *dest, uint32_t n);
+RADIANT_API int String_Compare(const char *a, const char *b);
+RADIANT_API int String_ToInt(char *number);
+
+#endif
---
+++ src/String.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+bool String_Equal(const char *a, const char *b) {
+  return (strcmp(a, b) == 0);
+}
+
+void String_Copy(const char *src, char *dest) {
+  uint32_t i = 0;
+
+  while (src[i] != '\0') {
+    dest[i] = src[i];
+    i++;
+  }
+}
+
+/* TODO: implement */
+void String_CopyN(const char *src, char *dest, uint32_t n);
+
+/* TODO: implement */
+int String_Compare(const char *a, const char *b);
+
+
+int String_ToInt(char *string) {
+  int x = 0;
+  char *c;
+  for (c = string; *c != '\0'; c++) {
+    if (*c < '0' || *c > '9') {
+      printf("Error - cannot convert string to int, invalid character '%c'\n", *c);
+      exit(-1);
+    }
+    x = (x + (*c - '0')) * 10;
+  }
+
+  return x;
+}
